43702af75bca9d7c9cda9d9bec9e0e79b838ea42
Francis <2330543226@qq.com>
2019-07-03 16:19:59 +0800

    c

--- src/main/java/restaurant/service/managerService/ManagerService.java
+++ src/main/java/restaurant/service/managerService/ManagerService.java
@@ -1,5 +1,17 @@
 package restaurant.service.managerService;
-
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import restaurant.repository.PersonRepository;
+import restaurant.utils.WebUtils;
 public class ManagerService {
-
+	
+	@Autowired
+	PersonRepository personDAO;
+	
+	public Object getPerson(){
+		Map<String,Object> map=new HashMap<String,Object>();
+		map.put("person",personDAO.findAll());
+		return 	WebUtils.setModelAndView("manage_check", map);
+	}
 }
--- src/main/webapp/WEB-INF/jsp/manage_check.jsp
+++ src/main/webapp/WEB-INF/jsp/manage_check.jsp
@@ -1,9 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>餐厅经理主页</title>
-<<<<<<< HEAD
 
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/bootstrap.min.css">
